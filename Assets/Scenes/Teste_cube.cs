a054bf004c9e434fe9ddd911b01878ae5febe4d7
alvinbonaventuresanchez-debug <alvin.bonaventure.sanchez@gmail.com>
2026-05-12 14:34:25 +0200

    V1.5.2

--- Assets/Scenes/Teste_cube.cs
+++ Assets/Scenes/Teste_cube.cs
@@ -14,5 +14,8 @@ public class Teste_cube : MonoBehaviour
         // Faire grossir et rapetisser le cube
         float scale = 1 + 0.5f * Mathf.Sin(Time.time * 2);
         transform.localScale = Vector3.one * scale;
+
+        // Faire tourner le cube sur lui-même
+        transform.Rotate(Vector3.up * 50 * Time.deltaTime);
     }
 }
