1c273529c300ac63551075a256b5dcb2cf410de7
alvinbonaventuresanchez-debug <alvin.bonaventure.sanchez@gmail.com>
2026-05-12 14:18:33 +0200

    V1.5.1

---
+++ Assets/Scenes/Teste_cube.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Teste_cube : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Faire grossir et rapetisser le cube
+        float scale = 1 + 0.5f * Mathf.Sin(Time.time * 2);
+        transform.localScale = Vector3.one * scale;
+    }
+}
