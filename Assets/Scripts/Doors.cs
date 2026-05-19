c94d5e64213e61fa0c22d6e7ee175030fbab39cc
Julianne ROGAM <juliannerogam@gmail.com>
2026-05-19 15:47:21 +0200

    maze cell

---
+++ Assets/Scripts/Doors.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider))]
+public class Doors : MonoBehaviour
+{
+    public GameObject door; 
+    public Vector3 openPosition = new Vector3(0, 120, 0);
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            Open();
+    }
+    void Open()
+    {
+        door.transform.localEulerAngles = openPosition;
+    }
+
+    void OntriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+            Close();
+    }
+    void Close()
+    {
+        door.transform.localEulerAngles = Vector3.zero;
+    }
+}
