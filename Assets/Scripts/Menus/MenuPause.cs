2d574361784a8aa6177e0c17c306e3998ebc8cde
romainbezombes-cloud <romain.bezombes@mmibordeaux.com>
2026-05-19 17:31:45 +0200

    menu pause options update

--- Assets/Scripts/Menus/MenuPause.cs
+++ Assets/Scripts/Menus/MenuPause.cs
@@ -4,17 +4,24 @@ using UnityEngine.InputSystem;
 public class MenuPause : MonoBehaviour
 {
     public GameObject panelPause;
+    public GameObject panelOption;
     void Start()
     {
         panelPause.SetActive(false);
+        panelOption.SetActive(false);
     }
-//fonctionalités des boutons (options,retour au menu,etc)
-/*
+//ouvrir et fermet les options
     public void OpenOptions(){
     Debug.Log("Open options");
     panelOption.SetActive(true);
     }
-*/
+
+    public void CloseOptions(){
+    Debug.Log("Close options");
+    panelOption.SetActive(false);
+    }
+
+
 
 
 //afficher/cacher le menu pause 
