24a11b4d6f93e7d3906aaebba89c1b5977383052
romainbezombes-cloud <romain.bezombes@mmibordeaux.com>
2026-05-19 12:38:55 +0200

    create menu pause

--- Assets/Scripts/Menus/MenuPause.cs
+++ Assets/Scripts/Menus/MenuPause.cs
@@ -5,7 +5,7 @@ public class MenuPause : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        izgedugz
     }
 
     // Update is called once per frame
